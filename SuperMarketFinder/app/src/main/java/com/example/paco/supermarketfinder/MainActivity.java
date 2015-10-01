1b51a695f5ccc00eac7689a659beb5a9d6d78189
fjfernandez93 <paco.fernandez.muelas@gmail.com>
2015-10-01 21:35:57 +0200

    Buttons finished
    —

--- SuperMarketFinder/app/src/main/java/com/example/paco/supermarketfinder/MainActivity.java
+++ SuperMarketFinder/app/src/main/java/com/example/paco/supermarketfinder/MainActivity.java
@@ -1,17 +1,97 @@
 package com.example.paco.supermarketfinder;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ImageButton b_mercadona;
+    private ImageButton b_dia;
+    private ImageButton b_supersol;
+    private ImageButton b_carrefour;
+    private ImageButton b_alcampo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b_mercadona = (ImageButton) findViewById(R.id.b_mercadona);
+        b_dia = (ImageButton) findViewById(R.id.b_dia);
+        b_supersol = (ImageButton) findViewById(R.id.b_supersol);
+        b_carrefour = (ImageButton) findViewById(R.id.b_carrefour);
+        b_alcampo = (ImageButton) findViewById(R.id.b_alcampo);
+
+        b_mercadona.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Uri gmmIntentUri = Uri.parse("geo:0,0?q=mercadona");
+               Intent mapIntent = new Intent(Intent.ACTION_VIEW,gmmIntentUri);
+               mapIntent.setPackage("com.google.android.apps.maps");
+               if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                   startActivity(mapIntent);
+               }
+           }
+        });
+
+        b_dia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=supermercado+dia");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW,gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
+
+        b_supersol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=supersol");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW,gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
+
+        b_carrefour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=carrefour");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW,gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
+
+        b_alcampo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=alcampo");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW,gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
+
+
+
+
     }
 
     @Override
@@ -29,7 +109,7 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.about) {
             return true;
         }
 
